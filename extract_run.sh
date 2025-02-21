270e8901ef1ec598968a9e13583b0e869ca5d11d
Moritz Bättig <moritz.baettig@bluewin.ch>
2025-02-21 16:53:24 +0100

    Fix extract_run.sh

--- extract_run.sh
+++ extract_run.sh
@@ -11,32 +11,32 @@ export ABSTRACT_SUPERGRAPH_CLUSTER=3
 export CLAIM_SUPERGRAPH_CLUSTER=3
 export EMBEDDING_BATCH=128
 
-python patent/cpc/cpc_phrase_list/cpc_phrase_list.py
-python patent/cpc/cpc_embedding/cpc_phrase_embedding_batch.py
-python patent/cpc/cpc_clustering/clustering.py
+venv/bin/python patent/cpc/cpc_phrase_list/cpc_phrase_list.py
+venv/bin/python patent/cpc/cpc_embedding/cpc_phrase_embedding_batch.py
+venv/bin/python patent/cpc/cpc_clustering/clustering.py
 
-python patent/title/title_candidate/candidate_synthesis.py
-python patent/title/title_embedding/title_phrase_embedding_batch.py
-python patent/title/title_graph/construct_graph.py
-python patent/title/title_score/title_metrics.py
-python patent/title/title_score/title_score_normalize.py
-python patent/title/title_rank/title_rank.py
-python patent/title/title_rank/title_to_text.py
-python patent/title/title_rank/title_selection.py
-python patent/title/title_clustering/title_clustering.py
+venv/bin/python patent/title/title_candidate/candidate_synthesis.py
+venv/bin/python patent/title/title_embedding/title_phrase_embedding_batch.py
+venv/bin/python patent/title/title_graph/construct_graph.py
+venv/bin/python patent/title/title_score/title_metrics.py
+venv/bin/python patent/title/title_score/title_score_normalize.py
+venv/bin/python patent/title/title_rank/title_rank.py
+venv/bin/python patent/title/title_rank/title_to_text.py
+venv/bin/python patent/title/title_rank/title_selection.py
+venv/bin/python patent/title/title_clustering/title_clustering.py
 
-python patent/abstract/abstract_candidate/candidate_synthesis.py
-python patent/abstract/abstract_embedding/abstract_phrase_embedding_batch.py
-python patent/abstract/abstract_graph/construct_graph.py
-python patent/abstract/abstract_score/abstract_metrics.py
-python patent/abstract/abstract_score/abstract_score_normalize.py
-python patent/abstract/abstract_rank/abstract_rank.py
-python patent/abstract/abstract_rank/abstract_to_text.py
-python patent/abstract/abstract_rank/abstract_selection.py
-python patent/abstract/abstract_clustering/abstract_clustering.py
+venv/bin/python patent/abstract/abstract_candidate/candidate_synthesis.py
+venv/bin/python patent/abstract/abstract_embedding/abstract_phrase_embedding_batch.py
+venv/bin/python patent/abstract/abstract_graph/construct_graph.py
+venv/bin/python patent/abstract/abstract_score/abstract_metrics.py
+venv/bin/python patent/abstract/abstract_score/abstract_score_normalize.py
+venv/bin/python patent/abstract/abstract_rank/abstract_rank.py
+venv/bin/python patent/abstract/abstract_rank/abstract_to_text.py
+venv/bin/python patent/abstract/abstract_rank/abstract_selection.py
+venv/bin/python patent/abstract/abstract_clustering/abstract_clustering.py
 
-python patent/claim/claim_candidate/candidate_synthesis.py
-python patent/claim/claim_embedding/claim_phrase_embedding_batch.py
+venv/bin/python patent/claim/claim_candidate/candidate_synthesis.py
+venv/bin/python patent/claim/claim_embedding/claim_phrase_embedding_batch.py
 python patent/claim/claim_graph/construct_graph.py
 python patent/claim/claim_score/claim_metrics.py
 python patent/claim/claim_score/claim_score_normalize.py
